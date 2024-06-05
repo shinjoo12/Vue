3b357821f3bca8a104fad4e2cc28d48841cb768d
shin <zhuyaan93@gmail.com>
2024-06-05 18:09:50 +0900

    6/5

---
+++ 09_VueBlogWithJson/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+    import TheWelcome from '@/components/home/TheWelcome.vue';
+</script>
+
+<template>
+    <main>
+        <TheWelcome />
+
+    </main>
+    
+</template>
+
+<style scoped>
+main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 70%;
+    margin-top: 1.5%;
+    margin-left: auto;
+    margin-right: auto;
+    border: 1px dotted;
+  }
+</style>
